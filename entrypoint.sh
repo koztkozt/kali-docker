6c6c929beb566b9aade361ebde0e53dbaa893fa6
koztkozt <96878659+koztkozt@users.noreply.github.com>
2022-01-08 14:49:34 +0800

    Remove SSL cert
    SSL cert no working with FoxyProxy

--- entrypoint.sh
+++ entrypoint.sh
@@ -21,24 +21,24 @@ fi
 
 # Start noVNC server
 
-if [ ! -f /etc/ssl/certs/novnc_cert.pem -o ! -f /etc/ssl/private/novnc_key.pem ]
-then
-  openssl req -new -x509 -days 365 -nodes \
-    -subj "/C=US/ST=IL/L=Springfield/O=OpenSource/CN=localhost" \
-    -out /etc/ssl/certs/novnc_cert.pem -keyout /etc/ssl/private/novnc_key.pem \
-    > /dev/null 2>&1
-fi
+# if [ ! -f /etc/ssl/certs/novnc_cert.pem -o ! -f /etc/ssl/private/novnc_key.pem ]
+# then
+#   openssl req -new -x509 -days 365 -nodes \
+#     -subj "/C=US/ST=IL/L=Springfield/O=OpenSource/CN=localhost" \
+#     -out /etc/ssl/certs/novnc_cert.pem -keyout /etc/ssl/private/novnc_key.pem \
+#     > /dev/null 2>&1
+# fi
 
-cat /etc/ssl/certs/novnc_cert.pem /etc/ssl/private/novnc_key.pem > /etc/ssl/private/novnc_combined.pem
-chmod 600 /etc/ssl/private/novnc_combined.pem
+# cat /etc/ssl/certs/novnc_cert.pem /etc/ssl/private/novnc_key.pem > /etc/ssl/private/novnc_combined.pem
+# chmod 600 /etc/ssl/private/novnc_combined.pem
 
 /usr/share/novnc/utils/launch.sh --listen $NOVNCPORT --vnc localhost:$VNCPORT \
-  --cert /etc/ssl/private/novnc_combined.pem --ssl-only \
+#   --cert /etc/ssl/private/novnc_combined.pem --ssl-only \
   > /var/log/novnc.log 2>&1 &
 
-echo "Launch your web browser and open https://localhost:9020/vnc.html"
-echo "Verify the certificate fingerprint:"
-openssl x509 -in /etc/ssl/certs/novnc_cert.pem -noout -fingerprint -sha256
+echo "Launch your web browser and open http://localhost:9020/vnc.html"
+# echo "Verify the certificate fingerprint:"
+# openssl x509 -in /etc/ssl/certs/novnc_cert.pem -noout -fingerprint -sha256
 
 # Start shell
 
